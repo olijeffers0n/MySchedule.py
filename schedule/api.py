572c5a1cc2cdc183f39695d5d869b35c8b542ae8
Zack Eckersley Pallett <40175773+ZackaryH8@users.noreply.github.com>
2023-07-31 10:23:29 +0100

    Add the parsing for timecard (#3)
    * Add the parsing for timecard
    
    ---------
    
    Co-authored-by: Ollie <69084614+olijeffers0n@users.noreply.github.com>

--- schedule/api.py
+++ schedule/api.py
@@ -1,25 +1,28 @@
 import requests
-import datetime
+from datetime import datetime, timedelta
 import bs4
 import pybase64
 from typing import List, Union
-from .structures import *
+from lxml import etree
+import re
+from .structures import Shift, Punch, PunchType, Clock
 
 DAYS = {0: "Mon.", 1: "Tues.", 2: "Weds.", 3: "Thurs.", 4: "Fri.", 5: "Sat.", 6: "Sun."}
 
 
 class ScheduleAPI:
-
     def __init__(self, username, password):
         self.username: str = username
         self.password: str = password
         self.auth_token: Union[str, None] = None
         self.data: Union[dict, None] = None
         self.session: requests.Session = requests.session()
-        self.session.headers.update({
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                          "(KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
-        })
+        self.session.headers.update(
+            {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                "(KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
+            }
+        )
 
     @staticmethod
     def get_week_code_for_date(date: datetime) -> str:
@@ -32,11 +35,13 @@ class ScheduleAPI:
 
     def get_current_week(self) -> int:
         req = self.session.get(
-            "https://mcduk.reflexisinc.co.uk/RWS4/ess/ess_emp_schedule.jsp?authToken=" + self.auth_token)
+            "https://mcduk.reflexisinc.co.uk/RWS4/ess/ess_emp_schedule.jsp?authToken="
+            + self.auth_token
+        )
         data = str(req.content)
         index = data.index("setGenShiftId")
 
-        return int(data[index + 14:index + data[index:].index(")")])
+        return int(data[index + 14 : index + data[index:].index(")")])
 
     @staticmethod
     def generate_date(date_string: str) -> datetime:
@@ -48,20 +53,27 @@ class ScheduleAPI:
         return f"{number // 60}:{minutes if len(minutes) == 2 else minutes + '0'}"
 
     def _create_form(self):
-        data = self.session.get("https://mcduk.reflexisinc.co.uk/kernel/views/authenticate/W/MCDUK.view")
+        data = self.session.get(
+            "https://mcduk.reflexisinc.co.uk/kernel/views/authenticate/W/MCDUK.view"
+        )
         # Use the lxml parser to parse the html
         html = bs4.BeautifulSoup(data.content, "html.parser")
-        other_stuff = html.find_all("div", {"id": "login-content"})[0].find_all("input", {"type": "hidden"})
+        other_stuff = html.find_all("div", {"id": "login-content"})[0].find_all(
+            "input", {"type": "hidden"}
+        )
 
         divs = []
 
-        for div in html.find_all("div", {"id": "primaryAuthenticationContainer"})[0].find_all("div"):
-
+        for div in html.find_all("div", {"id": "primaryAuthenticationContainer"})[
+            0
+        ].find_all("div"):
             clazz = div.get("class")[0] if div.get("class") is not None else ""
             ids = div.get("id")
 
-            if clazz in ["access-username", "access-password"] or \
-                    ids in ["useridBlankErrorMessage", "passwordBlankErrorMessage"]:
+            if clazz in ["access-username", "access-password"] or ids in [
+                "useridBlankErrorMessage",
+                "passwordBlankErrorMessage",
+            ]:
                 continue
 
             divs.append(div)
@@ -73,15 +85,18 @@ class ScheduleAPI:
             "hidden": "",
             "secureFormFields": True,
             username_div.find_all("input")[0].get("name"): self.username,
-            password_div.find_all("input")[1].get("name"): pybase64.encodebytes(self.password.encode("utf-8")).decode(
-                "utf-8")
+            password_div.find_all("input")[1]
+            .get("name"): pybase64.encodebytes(self.password.encode("utf-8"))
+            .decode("utf-8"),
         }
 
         for div in other_stuff:
             params[div.get("name")] = div.get("value")
 
-        login_req = self.session.post("https://mcduk.reflexisinc.co.uk/kernel/views/validateuserlogin.view",
-                                      data=params)
+        login_req = self.session.post(
+            "https://mcduk.reflexisinc.co.uk/kernel/views/validateuserlogin.view",
+            data=params,
+        )
 
         return login_req.content
 
@@ -92,19 +107,25 @@ class ScheduleAPI:
 
         data = {section.get("name"): section.get("value") for section in inputs}
         self.data = data
-        self.session.post("https://mcduk.reflexisinc.co.uk/kernel/auth/createLocalSession.rest", data=data)
+        self.session.post(
+            "https://mcduk.reflexisinc.co.uk/kernel/auth/createLocalSession.rest",
+            data=data,
+        )
 
         return data["authToken"]
 
     def _get_all_data(self):
-        data = self.session.post("https://mcduk.reflexisinc.co.uk/MYWORK/systemAction.htm", data={
-            "localeCode": self.data["localeCode"],
-            "authToken": self.auth_token,
-            "expiresOn": self.data["expiresOn"],
-            "operation": "logUserIn",
-            "domainId": self.data["domainId"],
-            "X-reflexis-header-X": self.data["X-reflexis-header-X"],
-        })
+        data = self.session.post(
+            "https://mcduk.reflexisinc.co.uk/MYWORK/systemAction.htm",
+            data={
+                "localeCode": self.data["localeCode"],
+                "authToken": self.auth_token,
+                "expiresOn": self.data["expiresOn"],
+                "operation": "logUserIn",
+                "domainId": self.data["domainId"],
+                "X-reflexis-header-X": self.data["X-reflexis-header-X"],
+            },
+        )
 
         html = bs4.BeautifulSoup(data.content, "html.parser").decode()
 
@@ -113,11 +134,13 @@ class ScheduleAPI:
 
         variables = {}
 
-        for line in html[start + 46:end + len(html[:start])].split("\n"):
+        for line in html[start + 46 : end + len(html[:start])].split("\n"):
             if line.strip() == "":
                 continue
             new_line = line.strip().replace("var ", "").replace(";", "")
-            variables[new_line.split("=")[0].strip()] = new_line.split("=")[1].strip().replace("'", "")
+            variables[new_line.split("=")[0].strip()] = (
+                new_line.split("=")[1].strip().replace("'", "")
+            )
 
         self.data.update(variables)
 
@@ -136,10 +159,15 @@ class ScheduleAPI:
         if self.data is None:
             raise Exception("You must login before you can get shifts")
 
-        self.session.get("https://mcduk.reflexisinc.co.uk/RWS4/ess/ess_emp_schedule.jsp?authToken=" + self.auth_token)
-        req = self.session.get(f"https://mcduk.reflexisinc.co.uk/RWS4/controller/ess/map/{self.data['storeId']}/"
-                               f"{week_code}"
-                               f"/requestswithshiftsdata.json?authToken={self.auth_token}")
+        self.session.get(
+            "https://mcduk.reflexisinc.co.uk/RWS4/ess/ess_emp_schedule.jsp?authToken="
+            + self.auth_token
+        )
+        req = self.session.get(
+            f"https://mcduk.reflexisinc.co.uk/RWS4/controller/ess/map/{self.data['storeId']}/"
+            f"{week_code}"
+            f"/requestswithshiftsdata.json?authToken={self.auth_token}"
+        )
 
         if req.status_code != 200:
             raise Exception("Failed to get shifts")
@@ -150,9 +178,15 @@ class ScheduleAPI:
 
             date_obj = self.generate_date(date)
 
-            shifts.append(Shift(date_obj, self.time_from_number(data[0]["startTime"]),
-                                self.time_from_number(data[0]["endTime"]),
-                                self.time_from_number(data[0]["duration"]), data))
+            shifts.append(
+                Shift(
+                    date_obj,
+                    self.time_from_number(data[0]["startTime"]),
+                    self.time_from_number(data[0]["endTime"]),
+                    self.time_from_number(data[0]["duration"]),
+                    data,
+                )
+            )
 
         return shifts
 
@@ -162,3 +196,133 @@ class ScheduleAPI:
         :return: A list of shifts for the given date
         """
         return self.get_shifts_for_week(int(self.get_week_code_for_date(date)))
+
+    @staticmethod
+    def extract_time(cdata):
+        """
+        :param cdata: The CDATA content to extract the time from
+        :return: The time extracted from the CDATA content
+        """
+
+        time_match = re.search(r"\d{2}:\d{2}", cdata)
+        if time_match:
+            return time_match.group()
+        return None
+
+    def get_timecard(self, week_end_date: str = None) -> List[Clock]:
+        """
+        :param week_end_date: The string in format DD/MM/YYYY of the Sunday of the week to get the timecard for
+        :return: A timecard for the given week code
+        """
+        data = {
+                "authToken": self.auth_token,
+                "reqType": "ESS",
+                "mm": "ESSTIMECARD",
+                "sm": "SUMMARY",
+            }
+
+        if week_end_date is not None:
+            data.update({
+                "weekendDate": week_end_date,
+                "weekend": week_end_date,
+            })
+
+        # Make a request to the timecard page
+        tc_request = self.session.post(
+            "https://mcduk.reflexisinc.co.uk/RWS4/rta/tcard.jsp",
+            data=data,
+        )
+
+        # If the request failed, raise an exception
+        if tc_request.status_code != 200:
+            raise Exception("Failed to get timecard")
+
+        # Use BS4 to parse the HTML
+        parsed_html = bs4.BeautifulSoup(tc_request.content, "html.parser").decode()
+
+        # Extract the XML content from the JavaScript variable
+        punch_variable_match = re.search(
+            r"testXMLLoad\s*=\s*\'(.*?)\';", parsed_html, re.DOTALL
+        )
+        punch_xml_string = punch_variable_match.group(1)
+
+        # Remove all backslashes from the XML content
+        punch_xml_string = punch_xml_string.replace("\\", "")
+
+        # Remove XML declaration
+        punch_xml_string = punch_xml_string.replace(
+            '<?xml version="1.0" encoding="UTF-8"?>', ""
+        )
+
+        # print(punch_xml_string)
+
+        # Parse the XML content
+        tree = etree.fromstring(punch_xml_string)
+
+        # Extract the HH:MM and punch type from each CDATA content and store in a list of dictionaries
+        data_dict = {}
+        date = None
+
+        # Loop through each row in the XML
+        for row in tree.xpath("//row"):
+            date_nodes = row.xpath("cell[1]/text()")
+            time_nodes = row.xpath("cell[5]/text()")
+            punch_type_nodes = row.xpath("cell[3]/text()")
+
+            # Check if date is present, update the date variable and add a new entry to the data dictionary
+            if date_nodes and date_nodes[0] != " ":
+                date = date_nodes[0]
+                data_dict[date] = {"punches": []}
+            
+            # Check if time and punch is present
+            if time_nodes and punch_type_nodes:
+                # Extract the time and punch type from the CDATA content
+                time = self.extract_time(time_nodes[0])
+
+                punch_type = punch_type_nodes[0]
+
+                # Add the time and punch type as a new entry
+                data_dict[date]["punches"].append({"time": time, "type": punch_type})
+
+        clocks = []
+
+        for date, data in data_dict.items():
+            punches = []
+            time_clocked_in = timedelta()
+            time_clocked_out = timedelta()
+            clock_in_time = None
+            break_time = None
+
+            for punch in data["punches"]:
+                punches.append(Punch(PunchType(punch["type"]), punch["time"]))
+
+                if punch["type"] == "Shift Start":
+                    clock_in_time = datetime.strptime(punch["time"], "%H:%M")
+
+                if punch["type"] == "Meal Start" or punch["type"] == "Shift End":
+                    diff = datetime.strptime(punch["time"], "%H:%M") - clock_in_time
+                    time_clocked_in += diff
+                    break_time = datetime.strptime(punch["time"], "%H:%M")
+
+                if punch["type"] == "Meal End":
+                    diff = datetime.strptime(punch["time"], "%H:%M") - break_time
+                    time_clocked_out += diff
+                    clock_in_time = datetime.strptime(punch["time"], "%H:%M")
+
+            # Convert total seconds to hours and minutes
+            time_clocked_in_hours, remaining_seconds = divmod(time_clocked_in.seconds, 3600)
+            time_clocked_in_minutes = remaining_seconds // 60
+
+            time_clocked_out_hours, remaining_seconds = divmod(time_clocked_out.seconds, 3600)
+            time_clocked_out_minutes = remaining_seconds // 60
+
+            clocks.append(
+                Clock(
+                    date,
+                    punches,
+                    f"{time_clocked_in_hours}:{time_clocked_in_minutes}",
+                    f"{time_clocked_out_hours}:{time_clocked_out_minutes}"
+                )
+            )
+
+        return clocks
--- schedule/structures.py
+++ schedule/structures.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from typing import Dict, Any
+from typing import Dict, Any, List
+from enum import Enum
 import json
 
 
 class Shift:
-
     def __init__(self, date, start_time, end_time, duration, raw):
         self._date = date
         self._start_time = start_time
@@ -32,10 +32,46 @@ class Shift:
     def raw_data(self) -> Dict[str, Any]:
         return self._raw
 
-    def __repr__(self):
-        return "Shift("+json.dumps({
-            "date": self.date.strftime("%d/%m/%Y"),
-            "start_time": self.start_time,
-            "end_time": self.end_time,
-            "duration": self.duration
-        }) + ")"
+    def __repr__(self) -> str:
+        return (
+            "Shift("
+            + json.dumps(
+                {
+                    "date": self.date.strftime("%d/%m/%Y"),
+                    "start_time": self.start_time,
+                    "end_time": self.end_time,
+                    "duration": self.duration,
+                }
+            )
+            + ")"
+        )
+
+
+class PunchType(Enum):
+    ShiftStart = "Shift Start"
+    BreakStart = "Meal Start"
+    BreakEnd = "Meal End"
+    ShiftEnd = "Shift End"
+
+
+class Punch:
+    def __init__(self, punch_type: PunchType, time: str) -> None:
+        self.type = punch_type
+        self.time = time
+
+    def __repr__(self) -> str:
+        return f"Punch({self.type}, {self.time})"
+
+
+class Clock:
+    def __init__(self, date: str, punches: List[Punch], clocked_in_time: str, break_time: str) -> None:
+        self.date = date
+        self.punches = punches
+        self.time_clocked_in = clocked_in_time
+        self.time_clocked_out = break_time
+
+    def __repr__(self) -> str:
+        data = "Clock("
+        for punch in self.punches:
+            data += punch.__repr__() + ", "
+        return data + f"Time Clocked in: {self.time_clocked_in}, Time Clocked out: {self.time_clocked_out}" + ")"
